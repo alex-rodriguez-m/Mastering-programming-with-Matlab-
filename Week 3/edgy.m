20eee59da5f7af7f1393b72762dc0e8e4b7f64ef
Alejandro Rodriguez <65473436+AlexGTL2602@users.noreply.github.com>
2020-09-21 18:49:05 -0500

    Add files via upload

---
+++ Week 3/edgy.m
@@ -0,0 +1,19 @@
+function output =edgy(cc)
+C=double(cc);
+
+
+for i=1:size(C,1)-2
+    for j=1:size(C,2)-2
+        %Sobel mask for x-direction:
+        Gx=((2*C(i+2,j+1)+C(i+2,j)+C(i+2,j+2))-(2*C(i,j+1)+C(i,j)+C(i,j+2)));
+        %Sobel mask for y-direction:
+        Gy=((2*C(i+1,j+2)+C(i,j+2)+C(i+2,j+2))-(2*C(i+1,j)+C(i,j)+C(i+2,j)));
+     
+        %The gradient of the image
+        %B(i,j)=abs(Gx)+abs(Gy);
+        B(i,j)=sqrt(Gx.^2+Gy.^2);
+     
+    end
+end
+output = uint8(B)
+end 
