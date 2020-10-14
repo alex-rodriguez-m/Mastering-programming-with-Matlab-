a760ea86c2a7dc344f8430c04879e20e9bd923bb
Alejandro Rodriguez <65473436+AlexGTL2602@users.noreply.github.com>
2020-10-14 18:41:26 -0500

    Add files via upload

---
+++ Week 2/poly_fun.m
@@ -0,0 +1,9 @@
+function fh = poly_fun(p)
+  function polynomial = poly(x)
+    polynomial = 0; 
+    for ii = 1:length(p)
+      polynomial = polynomial + p(ii) .* x.^(ii-1);
+    end
+  end
+  fh = @poly;
+end
